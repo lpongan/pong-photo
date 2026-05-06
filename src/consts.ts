b7e95ec0411a7cd5a607cd566543eace22ed7a32
lpongan <lpongan@gmail.com>
2026-05-05 22:35:13 -0400

    portfolio updates and logo

--- src/consts.ts
+++ src/consts.ts
@@ -5,7 +5,7 @@ export const SITE_DESCRIPTION = "Photorgraphy that's honest, unpretentious, and 
 export const SITE_AUTHOR = "Leo Pongan";
 
 /** CDN base URLs for remote images. */
-export const CDN_FAVICON_URL = "https://res.cloudinary.com/dvwl2ci5j/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1777951052/isang-bagsak-bw-v2_ryrsrc.png";
+export const CDN_FAVICON_URL = "https://res.cloudinary.com/dvwl2ci5j/image/upload/v1778033288/balisun_marjem.png";
 export const CDN_COVER_IMAGE_URL = "https://res.cloudinary.com/dvwl2ci5j/image/upload/v1777950985/DSC08327_ktm6kk.jpg";
 
 /** Social media links. */
