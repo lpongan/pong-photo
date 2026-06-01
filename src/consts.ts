96c3deefadad24f97ba43ee73334e9df3d4572c1
lpongan <lpongan@gmail.com>
2026-06-01 08:03:54 -0400

    adds ga tag

--- src/consts.ts
+++ src/consts.ts
@@ -4,6 +4,9 @@ export const SITE_TITLE = "get ponged";
 export const SITE_DESCRIPTION = "Photorgraphy that's honest, unpretentious, and a little bit weird.";
 export const SITE_AUTHOR = "Leo Pongan";
 
+/** Google Analytics measurement ID. */
+export const GA_MEASUREMENT_ID = "G-SCCCK4ZJBL";
+
 /** CDN base URLs for remote images. */
 export const CDN_FAVICON_URL = "https://res.cloudinary.com/dvwl2ci5j/image/upload/v1778033288/balisun_marjem.png";
 export const CDN_COVER_IMAGE_URL = "https://res.cloudinary.com/dvwl2ci5j/image/upload/v1777950985/DSC08327_ktm6kk.jpg";
